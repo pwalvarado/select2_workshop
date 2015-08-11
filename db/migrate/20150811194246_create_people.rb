54266270b5ef4e7e96ba275f9960654404f97a1c
Pedro Alvrado <pwalvarado@gmail.com>
2015-08-11 14:50:33 -0500

    Migrating db: rake db:migrate and seeding

--- db/migrate/20150811194246_create_people.rb
+++ db/migrate/20150811194246_create_people.rb
@@ -2,9 +2,16 @@ class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
       t.string :name
-      t. :favorite_color
+      t.string :favorite_color
 
       t.timestamps null: false
     end
+    Person.create name: "Pedro",     favorite_color: "Yellow"
+    Person.create name: "Valentina", favorite_color: "Blue"
+    Person.create name: "Ruben",     favorite_color: "Cyan"
+    Person.create name: "Andres",    favorite_color: "Brown"
+    Person.create name: "Christian", favorite_color: "Magenta"
+    Person.create name: "Camilo",    favorite_color: "Red"
+    Person.create name: "MArta",     favorite_color: "Green"
   end
 end
