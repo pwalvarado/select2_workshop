8f6abd07836c28db9b37edcdf0b05b0527163353
Pedro Alvrado <pwalvarado@gmail.com>
2015-08-11 16:44:30 -0500

    Simple select2 example

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,7 +4,8 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.json
   def index
-    @people = Person.all
+    @people = Person.order('name ASC').all
+    respond_to :json, :html
   end
 
   # GET /people/1
